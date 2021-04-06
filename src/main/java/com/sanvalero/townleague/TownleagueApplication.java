d00cfb2f3f61735793bfa6a046ab81260a5492d6
S3b97 <GitHubChivito123>
2021-04-06 13:41:05 +0200

    Initial commit

---
+++ src/main/java/com/sanvalero/townleague/TownleagueApplication.java
@@ -0,0 +1,13 @@
+package com.sanvalero.townleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownleagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanvalero/townleague/TownleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanvalero.townleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
